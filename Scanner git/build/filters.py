a70372570e277ecda4b346bb17d7e6c0be900e3d
Pranav <pranavarjun14@gmail.com>
2021-10-23 00:10:28 +0530

    Scanner 1 2 3 4 added

--- Scanner git/build/filters.py
+++
@@ -1,8 +0,0 @@
-# import django_filters
-# from .models import *
-
-# class TrackerScannerFilter(django_filters.FilterSet):
-#     class Meta:
-#         model = TrackerScanner
-#         fields = '__all__'
-#         exclude = ['first_name']
